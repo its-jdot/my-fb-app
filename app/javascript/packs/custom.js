e2a277060bd51b8431b61b71502ae7fd1eb35b5b
jamshad <j.arshad1285@gmail.com>
2023-09-25 14:13:19 +0500

    Initial Commit

---
+++ app/javascript/packs/custom.js
@@ -0,0 +1,27 @@
+window.showAlert = function (){
+    alert("TEST")
+}
+
+
+window.showAlert = function (){
+    alert("Hello this is an Alert. Testing Javascript")
+}
+
+
+function showAlert() {
+    alert("TEST")
+  }
+
+console.log("Hello is Javascrip Testing (Custom file) ")
+
+
+
+function showAlert1() {
+    alert('Button clicked!');
+}
+
+// Add a click event listener to the button element
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', showAlert1);
+});
