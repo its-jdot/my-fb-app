9ae2de4304e002613330e14515afa8518a61b13b
jamshad <j.arshad1285@gmail.com>
2023-10-02 17:47:54 +0500

    Add Comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  
-  get 'posts/index'
-  get 'posts/new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
